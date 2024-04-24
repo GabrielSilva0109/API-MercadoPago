f0301ffec1100454f2b0bf3f105d6a8f987d39b2
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-04-24 10:56:29 -0300

    Update apimercadopago.py

--- apimercadopago.py
+++ apimercadopago.py
@@ -1,1 +1,14 @@
 import mercadopago
+
+sdk = mercadopago.SDK("TEST-28c8f574-ec33-4964-9bdd-9701238837bb")
+
+payment_data = {
+    "transaction_amount": 100,
+    
+}
+
+result = sdk.payment().create(payment_data)
+
+payment = result["response"]
+
+print(payment)
