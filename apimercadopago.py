2b49655dfbcb1dbe67f4394d0c13583ee67f2e4d
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-06-12 13:45:30 -0300

    Update apimercadopago.py

--- apimercadopago.py
+++ apimercadopago.py
@@ -1,6 +1,6 @@
 import mercadopago
 
-sdk = mercadopago.SDK("TEST-28c8f574-ec33-4964-9bdd-9701238837bb")
+sdk = mercadopago.SDK("APP_USR-d183076f-2c11-4f43-940c-b8632db9582b")
 
 payment_data = {
     "transaction_amount": 100,
